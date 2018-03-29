8dcb53123c27887812917470f11076ec9a60c29f
Yegorov <yegorov0725@yandex.ru>
2018-03-29 12:03:20 +0300

    Add highlight for task state from the whole list of states

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,8 @@
 module TasksHelper
+  def state_line(all_states, current_state)
+    all_states.each do |state|
+      badge_class = state == current_state ? 'badge-primary' : 'badge-secondary'
+      concat(content_tag('span', state, class: "badge #{badge_class} mr-2"))
+    end
+  end
 end
