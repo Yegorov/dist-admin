57408fc5b5c0314c6351765b2b99a796eceec1c9
Yegorov <yegorov0725@yandex.ru>
2018-04-04 16:55:07 +0300

    Add default_url_options host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_controller.default_url_options = { host: 'distadmin.donetsk.space' }
+  config.action_mailer.default_url_options = { host: 'distadmin.donetsk.space' }
 end
